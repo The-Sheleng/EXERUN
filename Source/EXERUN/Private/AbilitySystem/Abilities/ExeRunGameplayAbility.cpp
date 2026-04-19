97843f82b7cefec38969a4aee83ee1768477247d
Andrii Shelengovkyi <thesheleng@gmail.com>
2026-04-20 01:10:35 +0200

    Added AbilityComponents

--- Source/EXERUN/Private/AbilitySystem/Abilities/ExeRunGameplayAbility.cpp
+++ Source/EXERUN/Private/AbilitySystem/Abilities/ExeRunGameplayAbility.cpp
@@ -1,9 +1,101 @@
 // © 2026 Andrii Shelenhivskyi. Licensed under Creative Commons Attribution-NonCommercial 4.0.
 
-
 #include "AbilitySystem/Abilities/ExeRunGameplayAbility.h"
+
+#include "AbilitySystem/AbilityComponents/AbilityComponent.h"
 
 UExeRunGameplayAbility::UExeRunGameplayAbility()
 {
+	ReplicationPolicy = EGameplayAbilityReplicationPolicy::ReplicateNo;
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+	NetSecurityPolicy = EGameplayAbilityNetSecurityPolicy::ClientOrServer;
+}
+
+bool UExeRunGameplayAbility::CommitAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, 
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	FGameplayTagContainer* OptionalRelevantTags)
+{
+	const bool Result =  Super::CommitAbility(Handle, ActorInfo, ActivationInfo, OptionalRelevantTags);
+	
+	for (UAbilityComponent* Component : AbilityComponents)
+	{
+		Component->OnCommitAbility(Handle, ActorInfo, ActivationInfo, OptionalRelevantTags);
+	}
+	
+	return Result;
+}
+
+void UExeRunGameplayAbility::PreActivate(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, 
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	FOnGameplayAbilityEnded::FDelegate* OnGameplayAbilityEndedDelegate, 
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::PreActivate(Handle, ActorInfo, ActivationInfo, OnGameplayAbilityEndedDelegate, TriggerEventData);
+
+	for (UAbilityComponent* Component : AbilityComponents)
+	{
+		Component->OnPreActivate(Handle, ActorInfo, ActivationInfo, OnGameplayAbilityEndedDelegate, TriggerEventData);
+	}
+}
+
+void UExeRunGameplayAbility::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, 
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	if (bAutoCommitAbility && !CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		CancelAbility(Handle, ActorInfo, ActivationInfo, bReplicateCancelAutoCommitAbility);
+		return;
+	}
+	
+	if (bAutoCommitAbility || !CommitCheck(Handle, ActorInfo, ActivationInfo))
+	{
+		for (UAbilityComponent* Component : AbilityComponents)
+		{
+			Component->OnActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+		}
+	}
+	
+	if (bAutoEndAbility)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateAutoEndAbility, false);
+	}
+}
+
+void UExeRunGameplayAbility::CancelAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, 
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateCancelAbility)
+{
+	for (UAbilityComponent* Component : AbilityComponents)
+	{
+		Component->OnCancelAbility(Handle, ActorInfo, ActivationInfo, bReplicateCancelAbility);
+	}
+	
+	Super::CancelAbility(Handle, ActorInfo, ActivationInfo, bReplicateCancelAbility);
+}
+
+void UExeRunGameplayAbility::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, 
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	for (UAbilityComponent* Component : AbilityComponents)
+	{
+		Component->OnEndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+	}
+	
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
 }
--- Source/EXERUN/Public/AbilitySystem/Abilities/ExeRunGameplayAbility.h
+++ Source/EXERUN/Public/AbilitySystem/Abilities/ExeRunGameplayAbility.h
@@ -6,6 +6,8 @@
 #include "Abilities/GameplayAbility.h"
 #include "ExeRunGameplayAbility.generated.h"
 
+class UAbilityComponent;
+
 /** Base gameplay ability used in ExeRun project */
 UCLASS()
 class EXERUN_API UExeRunGameplayAbility : public UGameplayAbility
@@ -16,26 +18,53 @@ public:
 	
 	UExeRunGameplayAbility();
 	
-	/**
-	* void UGameplayAbility_Crouch::ActivateAbility(
+	virtual bool CommitAbility(
+		const FGameplayAbilitySpecHandle Handle, 
+		const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, 
+		FGameplayTagContainer* OptionalRelevantTags = nullptr) override;
+	
+	virtual void CancelAbility(
+		const FGameplayAbilitySpecHandle Handle, 
+		const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, 
+		bool bReplicateCancelAbility) override;
+
+
+protected:
+
+	virtual void PreActivate(
+		const FGameplayAbilitySpecHandle Handle, 
+		const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, 
+		FOnGameplayAbilityEnded::FDelegate* OnGameplayAbilityEndedDelegate, 
+		const FGameplayEventData* TriggerEventData = nullptr) override;	
+	
+	virtual void ActivateAbility(
 		const FGameplayAbilitySpecHandle Handle,
 		const FGameplayAbilityActorInfo* ActorInfo, 
 		const FGameplayAbilityActivationInfo ActivationInfo,
-		const FGameplayEventData* TriggerEventData)
-		{
-			Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-			
-			if (!CommitAbility(Handle, ActorInfo, ActivationInfo) 
-				|| !ensureAlways(ActorInfo && ActorInfo->AvatarActor.IsValid()))
-			{
-				CancelAbility(Handle, ActorInfo, ActivationInfo, true);
-				return;
-			}
-			
-			// Your Code
-			
-			EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
-		}
+		const FGameplayEventData* TriggerEventData) override;
+	
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle, 
+		const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, 
+		bool bReplicateEndAbility, 
+		bool bWasCancelled) override;
 
-	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Components")
+	bool bAutoCommitAbility = false;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Components", meta=(EditCondition="bAutoCommitAbility"))
+	bool bReplicateCancelAutoCommitAbility = true;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Components")
+	bool bAutoEndAbility = false;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Components", meta=(EditCondition="bAutoEndAbility"))
+	bool bReplicateAutoEndAbility = true;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Components", Instanced, meta=(ShowOnlyInnerProperties))
+	TArray<TObjectPtr<UAbilityComponent>> AbilityComponents;
 };
